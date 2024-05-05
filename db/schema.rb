1b31c5c2871b244e2b6efeecc1d808c5fa098cd4
Arth Thakkar <arththakkar@gmail.com>
2024-05-05 16:17:15 -0400

    Authentication token added to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_195716) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_05_201301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_05_195716) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token"
   end
 
   create_table "votes", force: :cascade do |t|

c6d8d907ca94713c729f85f6da106354fea11be1
Arth Thakkar <arththakkar@gmail.com>
2024-05-05 17:50:24 -0400

    Session controller with all sessions API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    before_action :authenticate_user
+    
+    private
+    
+    def authenticate_user
+        @current_user = User.find_by(authentication_token: params[:authentication_token])
+        unless @current_user
+        render json: { errors: ['Unauthorized'] }, status: 401
+        end
+    end
 end
